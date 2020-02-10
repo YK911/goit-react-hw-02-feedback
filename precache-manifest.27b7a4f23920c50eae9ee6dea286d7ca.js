054f3cc07b1d9e6dac785ca979ef5f6e9f35249f
YK911 <jaroslavkositsa@gmail.com>
2020-02-10 22:03:20 +0200

    Updates

---
+++ precache-manifest.27b7a4f23920c50eae9ee6dea286d7ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d149ba52cb54fa28d16ddc165c7aa2f6",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "0e4da427783295707cde",
+    "url": "/goit-react-hw-02-feedback/static/css/main.559d1f17.chunk.css"
+  },
+  {
+    "revision": "f9da26c991c1f8ac7641",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9d10c81a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9d10c81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4da427783295707cde",
+    "url": "/goit-react-hw-02-feedback/static/js/main.57e663d8.chunk.js"
+  },
+  {
+    "revision": "123d81bea882563839a7",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.d87da01c.js"
+  }
+]);
